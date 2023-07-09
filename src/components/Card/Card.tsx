381e37d27c7efe99a9f390527641cbac76895f38
cadusouzar <cadusouzaribeiro@hotmail.com>
2023-07-08 21:51:34 -0300

    bug in projects solved

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -53,16 +53,16 @@ export const Card: React.FC<PropsCard> = ({ variantsTitulo, variantsLinguagem, t
               <Icon key={variantItemTitulo} variant={variantItemTitulo} width='w-16 ml-6 md:ml-5 md:w-12 lg:w-14 xl:w-16 3xl:w-20 3xl:ml-6'/>
             ))}
             <Label text={tituloCard} size='text-medium md:text-more-small lg:text-small xl:text-small 3xl:text-medium-small' padding='p-5 md:p-3 lg:p-0 xl:p-4 3xl:p-7'/>
-            <div className='h-28 md:p-2 lg:w-72 lg:mt-3 biglg:w-full xl:mt-2 3xl:p-0 3xl:mt-0 3xl:w-full'>
+            <div className='h-28 md:p-2 lg:w-72 lg:mt-3 biglg:w-full xl:-mt-1 xl:p-0 xl:h-32 xl:w-full 3xl:p-0 3xl:mt-0 3xl:w-full'>
               <Text text={text} size={size}/>
             </div>
             <motion.div
               ref={divRef}
               initial={{ y: 50, opacity: 0 }}
               animate={controls}
-              className='flex flex-wrap justify-center space-y-7 w-96 ml-1 mt-20 md:w-56 md:mt-4 md:space-y-3 lg:w-72 lg:mt-12 xl:mt-4 biglg:w-full 3xl:mt-10 3xl:w-full'
+              className='flex flex-wrap justify-center space-y-7 w-96 ml-1 mt-20 md:w-56 md:mt-4 md:space-y-3 lg:w-72 lg:mt-12 xl:mt-4 biglg:w-full xl:w-96 3xl:mt-10 3xl:w-full'
             >
-              <Label text={tituloLinguagens} size="text-small md:text-real-small lg:text-more-small xl:text-small 3xl:text-small 3xl:mb-2"/>
+              <Label text={tituloLinguagens} size="text-small md:text-real-small lg:text-more-small xl:text-small xl:-mt-6 3xl:text-small 3xl:mb-2"/>
               {variantsLinguagem.map((variantItemLinguagem: string) => (
                 <Icon key={variantItemLinguagem} variant={variantItemLinguagem} marginLeft='md:ml-2 3xl:ml-0' width='w-14 md:w-10 lg:w-10 xl:w-14 3xl:w-20'/>
               ))}
--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -5,12 +5,12 @@ export const Skills = () => {
   return(
     <>
       <NavTop variantIcon="mylogo" textLabel="Habilidades"/>
-      <div className="flex flex-col justify-center md:flex-row mt-20 md:flex 3xl:mt-4">
+      <div className="flex flex-col justify-center md:flex-row mt-20 md:flex xl:mt-4 3xl:mt-4">
           <Card variantsTitulo={['designer']} text='Eu valorizo ​​uma estrutura de conteúdo simples, padrões de design limpos e interações inteligentes.' tituloCard="Design" tituloLinguagens="Ferramentas de Projeto:" variantsLinguagem={['figma','google sites']} size='text-small md:text-real-small lg:text-more-small xl:text-more-small 3xl:text-very-small'/>
           <Card variantsTitulo={['frontend']} text='Desenvolver sites e interfaces é minha paixão, amo desenvolver coisas do zero e dar vida às ideias.' tituloCard="Front-end e frameworks" tituloLinguagens="Linguagens conhecidas:" variantsLinguagem={['html','css','react','typescript','angular','vuejs','tailwind','bootstrap']} size='text-small md:text-real-small lg:text-more-small xl:text-more-small 3xl:text-very-small'/>
           <Card variantsTitulo={['backend']} text='Meu nível de código não é tão alto, porém consigo ter autonomia para desenvolver, buscando sempre aprender.' tituloCard="Back-end, Banco de Dados e Versionamento" tituloLinguagens="Linguagens conhecidas:" variantsLinguagem={['csharp','nodejs','javascript','nestjs','sql','oracle','git']} size='text-small md:text-real-small lg:text-more-small xl:text-more-small 3xl:text-very-small'/>
       </div>
-      <div className="flex justify-center mb-4 mt-6 space-x-4 lg:space-x-20 lg:mb-8 biglg:-mt-10 3xl:-mt-12">
+      <div className="flex justify-center mb-4 mt-6 space-x-4 lg:space-x-20 lg:mb-8 biglg:-mt-10 xl:-mt-10 3xl:-mt-12">
         <LinkTo to='/aboutme'>
           <ButtonText variant="aboutme" text="Sobre mim"/>
         </LinkTo>
