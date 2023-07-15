b152512a3a85a7f3dac0b1a2daf101f43f00458e
cadusouzar <cadusouzaribeiro@hotmail.com>
2023-07-14 21:44:19 -0300

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import {ResetStyles} from "components";
 import { Rotas } from "routes";
 import { Tema } from "theme";
-
+import { Analytics } from '@vercel/analytics/react'
 export const App = () =>  {
   return (
+    <>
     <Tema>
       <ResetStyles/>
       <Rotas/>
     </Tema>
+    <Analytics debug={true}/>
+    </>
   );
 }
